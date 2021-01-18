3c4266d122f6f379176eaf1d139e72f305e0fffb
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 23:17:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   newarray = ["wow", "I", "am", "really", "learning"]
+   newarray = ["I", "wow", "am", "really", "learning"]
    revisedarray = newarray.shift
      newarray [0]
 end
